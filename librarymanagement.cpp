1e95d080cbe3e69a96bf2e44305ec0a80e7af12d
manoj-sampang <iloveump40@gmail.com>
2025-07-13 08:10:45 +0545

    update coee

--- librarymanagement.cpp
+++ librarymanagement.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
 using namespace std;
 class LibraryManagement {
-    int year;
+    string year;
     string title,author,id;
     public:
         Library() {
-            Year = 0;
+            Year = nullptr;
             title = nullptr;
             author = nullptr;
             id = nullptr;
@@ -19,19 +19,40 @@ class LibraryManagement {
             
 };
 
+class Register {
+    string username, password;
+    void 
+};
+
 class Login {
     string username;
     string password;
-    public:
         Login() {
-            username = 'no username';
-            password = 'no password';
+            username = "no username";
+            password = "no password";
         }
         void input() {
-
+            cout << "Permission Accessed"z << endl;
+            cout << "username: ";
+            getline(cin, username);
+            cout << "password: ";
+            getline(cin, password);
         }
+    protected:
+        void validity() {
+            string user, pass;
+            cout << "--- Validity Check ---" << endl;
+            cout << "username: ";
+            getline(cin, user);
+            cout << "pass: ";
+            getline(cin, pass);
+            if (username == user && password = pass) input();
+            else cout << "--- Access Denied, Enter your user and password again again ---" << endl;
+        }
+        
         
 };
 int main() {
     
+    return 0;
 }
